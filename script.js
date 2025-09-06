88abe176bcaf30257bf3e86519b06548141d6a14
imajdoch <98667979+imajdoch@users.noreply.github.com>
2025-09-06 15:07:40 -0400

    Create script.js
    js template

---
+++ script.js
@@ -0,0 +1,15 @@
+// Smooth scrolling for nav links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute("href")).scrollIntoView({
+      behavior: "smooth"
+    });
+  });
+});
+
+// Form submission alert
+document.querySelector("form").addEventListener("submit", e => {
+  e.preventDefault();
+  alert("Message sent! We'll get back to you soon.");
+});
