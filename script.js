e7393e7f5daeea2479218145d62e98d2f89a8f20
imajdoch <98667979+imajdoch@users.noreply.github.com>
2025-09-06 23:03:39 -0400

    Update script.js

--- script.js
+++ script.js
@@ -13,3 +13,13 @@ document.querySelector("form").addEventListener("submit", e => {
   e.preventDefault();
   alert("Message sent! We'll get back to you soon.");
 });
+
+
+const toggleBtn = document.getElementById('togglePDF');
+const pdf = document.getElementById('pdfEmbed');
+
+toggleBtn.addEventListener('click', () => {
+  pdf.classList.toggle('expanded');
+  toggleBtn.textContent = pdf.classList.contains('expanded') ? 'Collapse PDF' : 'View PDF';
+});
+
